443322191514ef2543ebb6468af3b0bc6df8c058
Hilario <hbarrenkua@protonmail.com>
2024-11-01 12:42:00 +0100

    Primer commit

---
+++ src/components/GridItem.jsx
@@ -0,0 +1,12 @@
+
+export const GridItem = ({ title, url}) => {
+  return (
+    <div className="card">
+
+        <img src={url} alt={title}>
+        </img>
+        <p>{title}</p>
+
+    </div>
+  )
+}
